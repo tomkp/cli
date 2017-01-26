6e2134821c2ea88e88d24ddcd6cdd9541a6c5d04
Matt Phillips <mattphillips1016@gmail.com>
2017-01-26 18:25:43 +0000

    Add option to create nameless files

--- src/tamm.js
+++ src/tamm.js
@@ -11,14 +11,14 @@ dot.templateSettings.strip = false;
 const dots = dot.process({ path: path.join(__dirname, '..', 'templates') });
 
 
-const statefulTemplate = (name, stylesheetSuffix) => dots.statefulTemplate({
-    stylesheetName: i.dasherize(i.underscore(name)),
+const statefulTemplate = (name, stylesheetName, stylesheetSuffix) => dots.statefulTemplate({
+    stylesheetName: stylesheetName,
     stylesheetSuffix: stylesheetSuffix,
     name: name,
 });
 
-const statelessTemplate = (name, stylesheetSuffix) => dots.statelessTemplate({
-    stylesheetName: i.dasherize(i.underscore(name)),
+const statelessTemplate = (name, stylesheetName, stylesheetSuffix) => dots.statelessTemplate({
+    stylesheetName: stylesheetName,
     stylesheetSuffix: stylesheetSuffix,
     name: name,
 });
@@ -32,6 +32,7 @@ program
     .arguments('<name>')
     .option('-t, --test', 'Log the generated components without any changes to the file system')
     .option('-s, --stateless', 'Create stateless React component')
+    .option('-n --nameless', 'Create js with index filename and css with style filename')
     .option('-v, --verbose')
     .option('--suffix <suffix>', 'The stylesheet suffix (css, scss, less etc...)')
     .action(function (name) {
@@ -39,15 +40,18 @@ program
 
         const stylesheetSuffix = program.suffix || 'css';
 
+        const jsFileName = program.nameless ? 'index' : name;
+        const stylesheetName = program.nameless ? 'style' : i.dasherize(i.underscore(name));
+
         const stylesheet = i.dasherize(i.underscore(name));
         const jsFileContents = program.stateless
-          ? statelessTemplate(name, stylesheetSuffix)
-          : statefulTemplate(name, stylesheetSuffix);
+          ? statelessTemplate(name, stylesheetName, stylesheetSuffix)
+          : statefulTemplate(name, stylesheetName, stylesheetSuffix);
 
         const stylesheetFileContents = stylesheetTemplate(name);
 
-        const jsFile = `${stylesheet}/${name}.js`;
-        const stylesheetFile = `${stylesheet}/${stylesheet}.${stylesheetSuffix}`;
+        const jsFile = `${stylesheet}/${jsFileName}.js`;
+        const stylesheetFile = `${stylesheet}/${stylesheetName}.${stylesheetSuffix}`;
 
         if (program.verbose || program.test) {
             console.log(`${'-'.repeat(50)}\n${jsFile}:\n${jsFileContents}\n`);
